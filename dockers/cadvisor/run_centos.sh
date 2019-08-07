68c3261835075a878212ee4bee941bad924e0121
Masayoshi Kokuryo <kokuryo.m@teldevice.co.jp>
2019-08-07 09:27:47 +0900

    added cadvisor

---
+++ dockers/cadvisor/run_centos.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+docker run \
+  --privileged=true \
+  --volume=/cgroup:/cgroup:ro \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:rw \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --publish=8080:8080 \
+  --detach=true \
+  --name=cadvisor \
+  google/cadvisor:latest
